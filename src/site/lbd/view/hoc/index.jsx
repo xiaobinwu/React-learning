a59fb0b059b29f92c8467e68c5647fe92a881eef
Billy Wu <billy.wu@hirede.com>
2016-12-21 16:39:49 +0800

    高阶组件demo2

--- src/site/lbd/view/hoc/index.jsx
+++ src/site/lbd/view/hoc/index.jsx
@@ -2,7 +2,7 @@ import './index.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
 
-//高阶组件－demo1
+//高阶组件－demo1（使用其他元素包裹）
 const MyContainer1 = (WrappedComponent) => class extends React.Component{
 		render(){
 			return(
@@ -25,8 +25,38 @@ const AppComponent1 = class extends React.Component {
 const HandledComponent1 =  MyContainer1(AppComponent1)
 
 
+//高阶组件-demo2（控制props）
+const MyContainer2 = (WrappedComponent) => class extends React.Component{
+		render(){
+			const newProps = {
+				hocText: '我是来自高阶组件的props Text',
+				color: 'red'
+			}
+			return(
+				<WrappedComponent {...this.props}  {...newProps}/>
+			) 
+		}
+	}
+const AppComponent2 = class extends React.Component {
+	render(){
+		return (
+			<div>
+				<p>我是原来组件的props</p>
+				<p>{this.props.text}</p>
+				<p>我是高阶组件的props</p>
+				<p>{this.props.hocText}</p>				
+				<p>我是经高阶组件改变的props</p>
+				<p>{this.props.color}</p>	
+			</div>
+		);
+	}
+}
+const HandledComponent2 =  MyContainer2(AppComponent2)
+
+
+
 
 ReactDom.render(
-	<HandledComponent1 />,
+	<HandledComponent2 text="我是来自身组件的props Text" color="green"/>,
 	document.getElementById('app')
 );
